bff4403398aa1a53908bd071b5e36e957deb04f3
robin van Lierde <robin2383@hotmail.com>
2016-12-14 15:06:13 +0100

    fouten in packages

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,17 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
-var CommentSchema = new mongoose.Schema({
-  body: String,
-  author: String,
-  upvotes: {type: Number, default: 0},
-  post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
-});
-
-mongoose.model('Comment', CommentSchema);
+passport.use(new LocalStrategy(function(username, password, done){
+  User.findOne({username: username}, function(err, user){
+    if(err){return done(err);}
+    if(!user){
+      return done(null, false, {message: 'Incorrect username.'});
+    }
+    if(!user.validPassword(password)){
+      return done(null, false, {message: 'Incorrect password.'});
+    }
+    return done(null, user);
+  });
+}));
