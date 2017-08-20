adb12a066e62a2252350f38c433b3bb0a50237f4
robin van Lierde <robin2383@hotmail.com>
2017-08-20 19:30:23 +0200

    adjusting gulp

--- gulpfile.js
+++ gulpfile.js
@@ -30,26 +30,10 @@ gulp.task('nodemon', function(cb) {
     }).on('restart');
 });
 
-gulp.task('develop', function () {
-    var stream = nodemon({ script: 'server.js'
-        , ext: 'html js'
-        , ignore: ['ignored.js']
-        , tasks: ['lint'] });
-
-    stream
-        .on('restart', function () {
-            console.log('restarted!')
-        })
-        .on('crash', function() {
-            console.error('Application has crashed!\n');
-            stream.emit('restart', 10)  // restart the server in 10 seconds
-        })
-});
-
 
 
 gulp.task('uglify',function(){
-    gulp.src('flappernews/public/javascripts/**/*.js')
+    gulp.src('flappernews/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('flappernews/minified'));
 });
